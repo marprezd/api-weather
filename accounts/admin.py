a2dc734bdf42d525ae86ed5d9ca35b9ad62356b2
Mario Perez <perezdhoz@gmail.com>
2021-01-12 15:33:38 -0500

    Register CustomUser and CustomUserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
+# api-weather/accounts/admin.py
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
+CustomUser = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    """This class extends from UserAdmin. Use the model and custom user forms.
+
+    Args:
+        UserAdmin ([type]): [description]
+    """
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username',]
+    
 # Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
