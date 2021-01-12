f88b90830555f129152100cef611771b57582748
Mario Perez <perezdhoz@gmail.com>
2021-01-12 16:17:15 -0500

    Add Custom User Tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,37 @@
+# api-weather/accounts/tests.py
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    """This class extends TestCase and tests whether users can be created with or
+    without administrative privileges.
+
+    Args:
+        TestCase ([type]): [description]
+    """
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='testuser',
+            email='testuser@testuser.com',
+            password='password12345'
+        )
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.email, 'testuser@testuser.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='testadmin',
+            email='testadmin@testadmin.com',
+            password='password12345'
+        )
+        self.assertEqual(admin_user.username, 'testadmin')
+        self.assertEqual(admin_user.email, 'testadmin@testadmin.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
