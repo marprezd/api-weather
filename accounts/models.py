667ec1f048325c3c0f237a9259c73ddbb2df75e8
Mario Perez <perezdhoz@gmail.com>
2021-01-12 15:41:13 -0500

    Create the custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
+# api-weather/accounts/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class CustomUser(AbstractUser):
+    """This is a custom user class that extends from 
+    AbstractUser.
+
+    Args:
+        AbstractUser ([type]): [description]
+    """
+    pass
