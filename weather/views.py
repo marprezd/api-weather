a5154a8d3112ba776ea80869fa3d335dbede63d7
Mario Perez <perezdhoz@gmail.com>
2021-01-22 11:51:38 -0500

    Create WeatherViewSet class

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# api-weather/weather/views.py
+from rest_framework import viewsets
+from .serializers import WeatherSerializer
+from .models import Weather
+from .permissions import IsAuthorOrReadOnly
+
 
 # Create your views here.
+class WeatherViewSet(viewsets.ModelViewSet):
+    queryset = Weather.objects.all()
+    serializer_class = WeatherSerializer
+    permission_classes = [IsAuthorOrReadOnly]
