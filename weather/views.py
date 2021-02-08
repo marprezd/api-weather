92f93ef4859e2ad03ab01965d418246b13d5a92f
Mario Perez <perezdhoz@gmail.com>
2021-02-08 14:37:02 -0500

    Setting permissions IsAuthenticatedOrReadOnly in views

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,6 @@
 # api-weather/weather/views.py
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import WeatherSerializer
 from .models import Weather
 from .permissions import IsAuthorOrReadOnly
@@ -9,4 +10,4 @@ from .permissions import IsAuthorOrReadOnly
 class WeatherViewSet(viewsets.ModelViewSet):
     queryset = Weather.objects.all()
     serializer_class = WeatherSerializer
-    permission_classes = [IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
