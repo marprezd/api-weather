2171fa75b3f0676232e90308028c1147aa8e240c
Mario Perez <perezdhoz@gmail.com>
2021-02-08 14:33:12 -0500

    Minor changes to the max_limit class attribute

--- weather/pagination.py
+++ weather/pagination.py
@@ -3,5 +3,5 @@ from rest_framework.pagination import LimitOffsetPagination
 
 
 class HeaderLimitOffsetPagination(LimitOffsetPagination):
-    # Overrides the value assigned to the max_limit class attribute with 6
-    max_limit = 6
+    # Overrides the value assigned to the max_limit class attribute with 10
+    max_limit = 10 
