1ed0fea5956439b6a32413e85048c2236044e7c4
Mario Perez <perezdhoz@gmail.com>
2021-02-16 15:50:58 -0500

    Adding WeatherFilter class

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,7 @@
 # api-weather/weather/views.py
 from rest_framework import viewsets
+from django_filters import rest_framework as filters
+from django_filters import CharFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import WeatherSerializer
 from .models import Weather
@@ -7,7 +9,18 @@ from .permissions import IsAuthorOrReadOnly
 
 
 # Create your views here.
+class WeatherFilter(filters.FilterSet):
+    city = CharFilter(field_name='city', lookup_expr='icontains')
+    country = CharFilter(field_name='country', lookup_expr='icontains') 
+    class Meta:
+        model = Weather
+        fields = ['city', 'country']
+
+
 class WeatherViewSet(viewsets.ModelViewSet):
     queryset = Weather.objects.all()
     serializer_class = WeatherSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    filter_class = WeatherFilter
+    search_fields = ('^city', '^country')
+    ordering_fields = ('city',)
